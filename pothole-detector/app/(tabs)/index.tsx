1466d358a3fa15acc215f4f43de4f8bc33282e7f
dylan <dylanb.connect@gmail.com>
2026-05-22 21:17:22 -0700

    added permission requests

--- pothole-detector/app/(tabs)/index.tsx
+++ pothole-detector/app/(tabs)/index.tsx
@@ -1,5 +1,18 @@
 import { Text, View } from 'react-native';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import * as Location from 'expo-location';
+
+useEffect(() => {
+    // your startup code goes here
+    const start = async () => {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            console.log('Permission denied');
+            return;
+        }
+    };
+    console.log("App started");
+}, []);
 
 export default function HomeScreen() {
   const [potholeCount, setPotholeCount] = useState(0);
