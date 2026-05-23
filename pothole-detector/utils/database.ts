d6da205a0d0ecfd6082e6f9660ba7e116841b596
dylan <dylanb.connect@gmail.com>
2026-05-22 23:11:40 -0700

    added function that collects all the data and puts into database

--- pothole-detector/utils/database.ts
+++ pothole-detector/utils/database.ts
@@ -1,7 +1,10 @@
 import * as SQLite from 'expo-sqlite';
+import {OrientedBurst} from "@/utils/orientedBurst";
+
+let db: SQLite.SQLiteDatabase;
 
 const initDatabase  = async () => {
-    const db = await SQLite.openDatabaseAsync('potholes.db');
+    db = await SQLite.openDatabaseAsync('potholes.db');
     await db.execAsync(`CREATE TABLE IF NOT EXISTS pothole_events (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         latitude REAL,
@@ -11,5 +14,18 @@ const initDatabase  = async () => {
         detected_at TEXT,
         synced INTEGER DEFAULT 0
     );`)
+
+
 }
+
+async function saveEvent(latitude: number, longitude: number, burst: OrientedBurst, detectAt: string): Promise<void> {
+    await db.runAsync(`INSERT INTO pothole_events (latitude, longitude, z_values, timestamps_ms, detected_at)
+        VALUES (?, ?, ?, ?, ?)`,
+        [latitude, longitude,
+            JSON.stringify(burst.z_values),
+            JSON.stringify(burst.timestamps_ms),
+            detectAt]);
+}
+
+export { initDatabase, saveEvent };
 
