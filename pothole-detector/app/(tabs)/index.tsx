2dcb7759486d168d04eb640f0f6def97835346ae
dylan <dylanb.connect@gmail.com>
2026-05-22 21:57:43 -0700

    added state variable logic to examine if the user is driving (faster than 15mph)

--- pothole-detector/app/(tabs)/index.tsx
+++ pothole-detector/app/(tabs)/index.tsx
@@ -5,6 +5,7 @@ import * as Location from 'expo-location';
 export default function HomeScreen() {
 
   const [potholeCount, setPotholeCount] = useState(0);
+  const [isDriving, setIsDriving] = useState(false);
     useEffect(() => {
         // your startup code goes here
         const start = async () => {
@@ -13,6 +14,19 @@ export default function HomeScreen() {
                 console.log('Permission denied');
                 return;
             }
+            await Location.watchPositionAsync(
+                { accuracy: Location.Accuracy.High, timeInterval: 1000 },
+                (location) => {
+                    const speed = location.coords.speed ?? 0; // m/s
+                    if (speed >= 6.7056) {
+                        console.log('Driving');
+                        setIsDriving(true);
+                    } else {
+                        console.log('Not driving');
+                        setIsDriving(false);
+                    }
+                }
+            );
         };
         start();
         console.log("App started");
@@ -20,7 +34,8 @@ export default function HomeScreen() {
 
   return (
       <View style ={{ flex: 1, backgroundColor: 'white'}}>
-        <Text>Pothole Detector {potholeCount}</Text>
+        <Text>Pothole Detector: {potholeCount}</Text>
+          <Text>Driving: {isDriving ? 'Yes' : 'No'}</Text>
       </View>
   );
 }
